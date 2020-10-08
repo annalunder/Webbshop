e13356f20dfb406037d24e68941f43321c87895d
Anna Lunder <anna_lunder@yahoo.se>
2020-10-08 14:51:47 +0200

    Changed enum

--- WebShop.Models/Order.cs
+++ WebShop.Models/Order.cs
@@ -7,7 +7,6 @@ namespace WebShop.Models
     public class Order
     {
         public string OrderNumber { get; set; }
-        // public string OrderStatus { get; set; }
         public DateTime OrderDate { get; set; }
 
         public bool SentForShipment { get; set; }
